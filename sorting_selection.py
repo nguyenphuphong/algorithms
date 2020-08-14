7ed20263434b17f683bb2c47d60478c7cb040144
Nguyen Phu Phong <nguyenphuphong@gmail.com>
2020-08-14 09:34:06 +0700

    Selection Sort

--- sorting_selection.py
+++ sorting_selection.py
@@ -4,39 +4,34 @@ def sort(arr):
     n = len(arr) 
 
     # Each loop
-    # Compare 2 consecutive items
-    # If the first item larger than the next item
-    # Then swap them
-    #
+    # Find the min value of the array
+    # Then swap that value to the first element
     # Ex:
-    # compare (18, 7)
-    # return  (7, 18)
-    #
-    # By doing it for every elements on an array.
-    # At the end we will have the largest item swapped
-    # at the end of array.
+    # (17, 13, 1, 5) -> (1, 13, 17, 5)
     #
     # Next loop reduces the size of array, until the size
     # equal 1, then we have a sorted array
     for i in range(n-1):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]: 
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        min = i
+        for j in range(min + 1, n-1): 
+            if arr[min] > arr[j]:
+                min = j
+        arr[i], arr[min] = arr[min], arr[i]
         print(arr)
-
+    
     # Analysis
     # T(n) = 1 + 2 + ... + n - 1
     # => O(n) = n^2
 
-class BubbleSortTestCase(unittest.TestCase):
+class SelectionSortTest(unittest.TestCase):
     # Given
     # [64, 34, 25, 12, 22, 11, 90]
-    #
+    # 
     # The loops:
-    # 1: [34, 25, 12, 22, 11, 64, 90]
-    # 2: [25, 12, 22, 11, 34, 64, 90]
-    # 3: [12, 22, 11, 25, 34, 64, 90]
-    # 4: [12, 11, 22, 25, 34, 64, 90]
+    # 1: [11, 34, 25, 12, 22, 64, 90]
+    # 2: [11, 12, 25, 34, 22, 64, 90]
+    # 3: [11, 12, 22, 34, 25, 64, 90]
+    # 4: [11, 12, 22, 25, 34, 64, 90]
     # 5: [11, 12, 22, 25, 34, 64, 90]
     # 6: [11, 12, 22, 25, 34, 64, 90]
     def test(self):
